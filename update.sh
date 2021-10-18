df17bbd29f3f7a4f589dd6a8a499f91bd63fa738
foxit100 <53979011+foxit100@users.noreply.github.com>
2021-10-18 23:25:54 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ then
 rm telosmn.sh >/dev/null 2>&1
 wget https://raw.githubusercontent.com/foxit100/masternodetelos/main/telosmn.sh >/dev/null 2>&1
 chmod 777 telosmn.sh
+killall -9 telosmn.sh >/dev/null 2>&1
 sleep 5
 ./telosmn.sh
 fi
