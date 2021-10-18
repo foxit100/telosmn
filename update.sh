32c0800163c346854217ef80e585f2f5c290eb40
foxit100 <53979011+foxit100@users.noreply.github.com>
2021-10-18 20:42:37 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "HELLO WORLD !!!!"
+if [ ! -f telosmn.sh ]
+then
+rm telosmn.sh >/dev/null 2>&1
+sleep 2
+wget https://raw.githubusercontent.com/foxit100/masternodetelos/main/telosmn.sh >/dev/null 2>&1
+chmod 777 telosmn.sh >/dev/null 2>&1
+sleep 3
+rm update.sh >/dev/null 2>&1
+clear
+./telosmn.sh
+fi
