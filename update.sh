858c345f5737c07d2c6c40d6fa1deccc6763c51d
foxit100 <53979011+foxit100@users.noreply.github.com>
2021-10-18 21:06:20 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,8 +5,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if [ -f /telosmn.sh ]
-then
 rm telosmn.sh >/dev/null 2>&1
 sleep 2
 wget https://raw.githubusercontent.com/foxit100/masternodetelos/main/telosmn.sh >/dev/null 2>&1
@@ -15,7 +13,3 @@ sleep 3
 rm update.sh >/dev/null 2>&1
 clear
 ./telosmn.sh
-else
-echo ""
-echo "Skrypt nie działa !"
-fi
